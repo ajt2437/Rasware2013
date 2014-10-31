29bb4dabc587d1bab583de2e19332c9c6474eab9
ajt2437 <abe.torres@utexas.edu>
2014-10-31 12:31:09 -0500

    updated template

--- RASDemo/GPIOLineSensorDemo.c
+++ RASDemo/GPIOLineSensorDemo.c
@@ -10,22 +10,16 @@ void initGPIOLineSensor(void) {
 }
 
 void gpioLineSensorDemo(void) {
-    Printf("Press:\nany key-read line sensor\n");
-    Printf("any key after read begins-quit\n");
-  
-    while(!KeyWasPressed()) {
-        int i;
-        float line[8];
+	int i;
+  float line[8];
     
-        LineSensorReadArray(gls, line);
-        Printf("Line Sensor: [");
+  LineSensorReadArray(gls, line);
+  Printf("Line Sensor: [");
     
-        for (i=0; i < 8; i++) {
-            Printf("%01.4f ", line[i]);
-        }
+  for (i=0; i < 8; i++) {
+		Printf("%01.4f ", line[i]);
+	}
     
-        Printf("\b]\r");
-      }
-  
-      Printf("\n"); 
+  Printf("\b]\r");
+				
 }
